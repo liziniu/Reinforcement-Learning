bc00d833495382188fd555b9e7b340fa518bbee3
Li Ziniu <374387855@qq.com>
2018-09-02 10:50:59 +0800

    Update markov_chain.py

--- Abstraction/markov_chain.py
+++ Abstraction/markov_chain.py
@@ -7,20 +7,20 @@ np.random.seed(1)
 tf.set_random_seed(1)
 
 # transition probability
-t_p = np.array([[0.3, 0.3, 0.2, 0.2],
-                [0.2, 0.2, 0.3, 0.3],
-                [0.7, 0.1, 0.1, 0.1],
-                [0.3, 0.1, 0.4, 0.2]])
+t_p = np.array([[0.1, 0.8, 0.05, 0.05],
+                [0.8, 0.1, 0.05, 0.05],
+                [0.05, 0.05, 0.1, 0.8],
+                [0.1, 0.1, 0.7, 0.1]])
 # 4 states with 4-dimension
 states = np.random.rand(4, 4) * 10
 # print(states)
 
 sess = tf.Session()
 s = tf.placeholder(tf.float32, [4, 4], name="states")
-l1 = tf.layers.dense(s, 20, activation=tf.nn.sigmoid, kernel_initializer=tf.random_normal_initializer(0.3, 1.0))
+l1 = tf.layers.dense(s, 20, activation=tf.nn.sigmoid, kernel_initializer=tf.random_normal_initializer(1, 1.0))
 l2 = tf.layers.dense(l1, 2, activation=tf.nn.relu, kernel_initializer=tf.random_normal_initializer(0.3, 1.0))
 
-opt = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+opt = tf.train.GradientDescentOptimizer(learning_rate=0.05)
 sess.run(tf.global_variables_initializer())
 
 # pair-wise distance
@@ -45,7 +45,12 @@ for i in range(100):
         print(sess.run(loss, {s: states}))
     sess.run(opt.minimize(loss), {s: states})
 
+plt.style.use('ggplot')
 result = sess.run(l2, {s: states})
+print(result)
 plt.figure()
-plt.scatter(result[:, 0], result[:, 1])
+plt.scatter(result[:, 0], result[:, 1], c=['red', 'red', 'blue', 'blue'])
+plt.title('Low Dimension Presentation of State')
 plt.show()
+
+
